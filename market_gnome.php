3ef1a591ba31e8f3dffa901b1e051885f6e467c6
barneycg <barney@mother>
2013-12-29 17:42:43 +0000

    latest code

--- market_gnome.php
+++ market_gnome.php
@@ -3,7 +3,7 @@
 /**
  * Market Gnome
  *
- * Copyright (c) 2012-2012, Barney Garrett.
+ * Copyright (c) 2012-2013, Barney Garrett.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -80,173 +80,182 @@ $client->add_cb('on_auth_failure', function($reason) {
 
 $client->add_cb('on_chat_message',function($stanza) {
 	global $client,$config,$ignore_list;
-	$pdo = new PDO("mysql:host=".$config['mysql']['host'].";dbname=".$config['mysql']['db_name'], $config['mysql']['user'], $config['mysql']['password']);
-	$item_sql = $pdo->prepare('select typeID from invTypes where typeName = :tn');
-	$system_sql = $pdo->prepare('select itemID from mapDenormalize where solarSystemID is NULL and typeID = 5 and itemName = :in');
-	$region_sql = $pdo->prepare('select itemID from mapDenormalize where regionID is NULL and typeID = 3 and itemName = :in');
 
-	$client->get_roster();
+
+	//$client->get_roster();
 	
 	$from=preg_replace('/\/.*$/', '', $stanza->from);
-	if((strlen($stanza->body) > 0) && (!in_array($from,$ignore_list))) {
+	if((strlen($stanza->body) > 0) && (!in_array($from,$ignore_list))) 
+	{
+		$pdo = new PDO("mysql:host=".$config['mysql']['host'].";dbname=".$config['mysql']['db_name'], $config['mysql']['user'], $config['mysql']['password']);
+		$item_sql = $pdo->prepare('select typeID from invTypes where typeName = :tn');
+		$system_sql = $pdo->prepare('select itemID from mapDenormalize where solarSystemID is NULL and typeID = 5 and itemName = :in');
+		$region_sql = $pdo->prepare('select itemID from mapDenormalize where regionID is NULL and typeID = 3 and itemName = :in');
+		
 		// echo back the incoming message
-		
-		$request = explode ( '$' , $stanza->body);
-		$rc = count($request);
-		$reqest_type = '';
-		$typeID = '';
-		$locationID ='';
-		$scount = 0;
-		$icount = 0;
-		$invalid=false;
+		if (preg_match('/^\?OTR/',$stanza->body))
+		{
+			$message = "**** Thank you for your request " . $from . " ****\nTurn off OTR you muppet !!!\n";
+        }
+		else	
+		{
+			$request = explode ( '$' , $stanza->body);
+			$rc = count($request);
+			$reqest_type = '';
+			$typeID = '';
+			$locationID ='';
+			$scount = 0;
+			$icount = 0;
+			$invalid=false;
 
-		switch ($rc)
-		{
-			case 1:
-				$item = trim($request[0]);
-				$loc = 'Jita';
-				$ival = $item_sql->execute(array(':tn'=>$item));
-				$locationName = "Default (Jita)";
-				while ($typeID_row = $item_sql->fetch())
-				{
-					if (empty($typeID))
+			switch ($rc)
+			{
+				case 1:
+					$item = trim($request[0]);
+					$loc = 'Jita';
+					$ival = $item_sql->execute(array(':tn'=>$item));
+					$locationName = "Default (Jita)";
+					while ($typeID_row = $item_sql->fetch())
 					{
-						$typeID=$typeID_row['typeID'];
-					}
-					else
-						$typeID.=",".$typeID_row['typeID'];
-					$icount++;
-				}
-				if ($icount != 1)
-				{
-					$message = "**** Thank you for your request " . $from . " ****\nThere was an error with your request Format is Item Name\$System Name\n";
-					if ($icount == 0)
-					{
-						$message .= "The specified Item was not found\n";
-					}
-					$invalid = true;
-				}
-				$locationID='30000142';
-				$request_type = 'solarsystem';
-				Break;
-			case 2:
-				$item = trim($request[0]);
-				$loc = trim($request[1]);
-				$ival = $item_sql->execute(array(':tn'=> $item));
-				$system_sql->execute(array(':in'=>$loc));
-				$locationName = $loc;
-				while ($typeID_row = $item_sql->fetch())
-				{
-					if (empty($typeID))
-					{
-						$typeID=$typeID_row['typeID'];
-					}
-					else
-						$typeID.=",".$typeID_row['typeID'];
-					$icount++;
-				}
-				while ($systemID_row = $system_sql->fetch())
-				{
-					$locationID=$systemID_row['itemID'];
-					$scount++;
-				}
-				if (($icount != 1) || ($scount != 1))
-				{
-					$message = "**** Thank you for your request " . $from . " ****\nThere was an error with your request Format is Item Name\$System Name\n";
-					if ($icount == 0)
-					{
-						$message .= "The specified Item was not found\n";
-					}
-					if ($scount ==0)
-					{
-						$message .= "The specified System was not found\nIf you have entered a Region name the format is Item Name\$R\$Region Name\n";
-					}
-					$invalid = true;
-				}
-				$request_type = 'solarsystem';
-				Break;
-			case 3:
-				$item = trim($request[0]);
-				$loc = trim($request[2]);
-				$type = trim($request[1]);
-				if (strlen($loc) === 1)
-				{
-					$message = "**** Thank you for your request " . $from . " ****\nThere was an error with your request Format is Item Name\$R\$System Name\n";
-					$message .= "The request type should be the 2nd parameter\n";
-					Break;
-				}
-				switch ($type)
-				{
-					case 'R':
-					case 'r':
-						$ival = $item_sql->execute(array(':tn'=>$item));
-						$region_sql->execute(array(':in'=>$loc));
-						$locationName = "Region - " . $loc;
-						while ($typeID_row = $item_sql->fetch())
+						if (empty($typeID))
 						{
-							if (empty($typeID))
-							{
-								$typeID=$typeID_row['typeID'];
-							}
-							else
-								$typeID.=",".$typeID_row['typeID'];
-							$icount++;
+							$typeID=$typeID_row['typeID'];
 						}
-						while ($regionID_row = $region_sql->fetch())
-						{
-							$locationID=$regionID_row['itemID'];
-							$scount++;
-						}
-						if (($icount != 1) || ($scount != 1))
-						{
-							$message = "**** Thank you for your request " . $from . " ****\nThere was an error with your request Format is Item Name\$System Name\n";
-							if ($icount == 0)
-							{
-								$message .= "The specified Item was not found\n";
-							}
-							if ($scount ==0)
-							{
-								$message .= "The specified Region was not found\n";
-							}
-							$invalid=true;
-						}
-						$request_type = 'region';
-						Break;
-					default:
-						
+						else
+							$typeID.=",".$typeID_row['typeID'];
+						$icount++;
+					}
+					if ($icount != 1)
+					{
 						$message = "**** Thank you for your request " . $from . " ****\nThere was an error with your request Format is Item Name\$System Name\n";
-						$message .= "Sorry that request type was not understood\n";
+						if ($icount == 0)
+						{
+							$message .= "The specified Item was not found\n";
+						}
+						$invalid = true;
+					}
+					$locationID='30000142';
+					$request_type = 'solarsystem';
+					Break;
+				case 2:
+					$item = trim($request[0]);
+					$loc = trim($request[1]);
+					$ival = $item_sql->execute(array(':tn'=> $item));
+					$system_sql->execute(array(':in'=>$loc));
+					$locationName = $loc;
+					while ($typeID_row = $item_sql->fetch())
+					{
+						if (empty($typeID))
+						{
+							$typeID=$typeID_row['typeID'];
+						}
+						else
+							$typeID.=",".$typeID_row['typeID'];
+						$icount++;
+					}
+					while ($systemID_row = $system_sql->fetch())
+					{
+						$locationID=$systemID_row['itemID'];
+						$scount++;
+					}
+					if (($icount != 1) || ($scount != 1))
+					{
+						$message = "**** Thank you for your request " . $from . " ****\nThere was an error with your request Format is Item Name\$System Name\n";
+						if ($icount == 0)
+						{
+							$message .= "The specified Item was not found\n";
+						}
+						if ($scount ==0)
+						{
+							$message .= "The specified System was not found\nIf you have entered a Region name the format is Item Name\$R\$Region Name\n";
+						}
+						$invalid = true;
+					}
+					$request_type = 'solarsystem';
+					Break;
+				case 3:
+					$item = trim($request[0]);
+					$loc = trim($request[2]);
+					$type = trim($request[1]);
+					if (strlen($loc) === 1)
+					{
+						$message = "**** Thank you for your request " . $from . " ****\nThere was an error with your request Format is Item Name\$R\$System Name\n";
+						$message .= "The request type should be the 2nd parameter\n";
 						Break;
-				}
-				Break;					
-			default:
-				$message = "**** Thank you for your request " . $from . " ****\nThere was an error with your request Format is Item Name\$System Name\n";
-				$message .= "Sorry the wrong number of parameters was supplied\n";
-				Break;
-		}
-		
-		if ( ($rc >= 1) && ($rc <=3) && (strlen($loc) != 1) && ($invalid == false))
-		{
-			$pdo = null;
-			$return = '';
-			$ch = curl_init();
-			$char_name = rawurlencode($config['eve-marketdata']['char_name']);
-			$curl_url = "http://api.eve-marketdata.com/api/item_prices2.txt?char_name=".$char_name."&type_ids=" . $typeID ."&".$request_type."_ids=" .$locationID ."&buysell=s&minmax=min";
-			curl_setopt($ch, CURLOPT_URL, $curl_url);
-			curl_setopt($ch, CURLOPT_HEADER, 0);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			$return = curl_exec($ch);
-			curl_close($ch);
-			preg_match('/[sb]\s+\d+\s+\d+\s+(\d+\.*\d*)\s+(.*)$/m',$return,$matches);
-			$price = $matches[1];
-			$price = number_format(floatval($price),2,'.',',');
-			$updated = $matches[2];
-			$from=preg_replace('/@.*$/','',$from);
-			$message = "**** Thank you for your request " . $from . " ****\nItem = " . $item . "\nLocation = " . $locationName . "\nPrice = " . $price . "\nLast updated : " . $updated ."\n";
+					}
+					switch ($type)
+					{
+						case 'R':
+						case 'r':
+							$ival = $item_sql->execute(array(':tn'=>$item));
+							$region_sql->execute(array(':in'=>$loc));
+							$locationName = "Region - " . $loc;
+							while ($typeID_row = $item_sql->fetch())
+							{
+								if (empty($typeID))
+								{
+									$typeID=$typeID_row['typeID'];
+								}
+								else
+									$typeID.=",".$typeID_row['typeID'];
+								$icount++;
+							}
+							while ($regionID_row = $region_sql->fetch())
+							{
+								$locationID=$regionID_row['itemID'];
+								$scount++;
+							}
+							if (($icount != 1) || ($scount != 1))
+							{
+								$message = "**** Thank you for your request " . $from . " ****\nThere was an error with your request Format is Item Name\$System Name\n";
+								if ($icount == 0)
+								{
+									$message .= "The specified Item was not found\n";
+								}
+								if ($scount ==0)
+								{
+									$message .= "The specified Region was not found\n";
+								}
+								$invalid=true;
+							}
+							$request_type = 'region';
+							Break;
+						default:
+
+							$message = "**** Thank you for your request " . $from . " ****\nThere was an error with your request Format is Item Name\$System Name\n";
+							$message .= "Sorry that request type was not understood\n";
+							Break;
+					}
+					Break;					
+				default:
+					$message = "**** Thank you for your request " . $from . " ****\nThere was an error with your request Format is Item Name\$System Name\n";
+					$message .= "Sorry the wrong number of parameters was supplied\n";
+					Break;
+			}
+
+			if ( ($rc >= 1) && ($rc <=3) && (strlen($loc) != 1) && ($invalid == false))
+			{
+				$return = '';
+				$ch = curl_init();
+				$char_name = rawurlencode($config['eve-marketdata']['char_name']);
+				$curl_url = "http://api.eve-marketdata.com/api/item_prices2.txt?char_name=".$char_name."&type_ids=" . $typeID ."&".$request_type."_ids=" .$locationID ."&buysell=s&minmax=min";
+				curl_setopt($ch, CURLOPT_URL, $curl_url);
+				curl_setopt($ch, CURLOPT_HEADER, 0);
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+				$return = curl_exec($ch);
+				curl_close($ch);
+				preg_match('/[sb]\s+\d+\s+\d+\s+(\d+\.*\d*)\s+(.*)$/m',$return,$matches);
+				$price = $matches[1];
+				$price = number_format(floatval($price),2,'.',',');
+				$updated = $matches[2];
+				$from=preg_replace('/@.*$/','',$from);
+				$message = "**** Thank you for your request " . $from . " ****\nItem = " . $item . "\nLocation = " . $locationName . "\nPrice = " . $price . "\nLast updated : " . $updated ."\n";
+			}
 		}
 				
 		$msg = new XMPPMsg(array('type'=>'chat', 'to'=>$stanza->from, 'from'=>$stanza->to), $message);
 		$client->send($msg);
+		$pdo = null;
 	}
 });
 
